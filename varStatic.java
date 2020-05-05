9b52aaaf391cb4334267dcee123b5600ff74ecb7
anfauziah <61958967+anfauziah@users.noreply.github.com>
2020-05-05 22:19:55 +0800

    Add files via upload

---
+++ varStatic.java
@@ -0,0 +1,11 @@
+package no2;
+
+public class varStatic {
+    public static void main(String[] args){
+        String firstName = "Azmi fauziah ";
+        String lastName = "Nur";
+        String FULLname = firstName + lastName;
+        
+        System.out.println(FULLname);
+    }
+}
